d2ecc1a47165dca41a8ff2b4df4052ed2ca95007
Elielson André <68979955+elielson-and@users.noreply.github.com>
2024-10-20 12:41:44 -0300

    comments

--- PokemonReviewApp/Models/Owner.cs
+++ PokemonReviewApp/Models/Owner.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Gym { get; set; }
-        public Country Country { get; set; }
+        public Country Country { get; set; } // An Owner Belongs to a Country
     }
 }
--- PokemonReviewApp/Models/Review.cs
+++ PokemonReviewApp/Models/Review.cs
@@ -5,8 +5,8 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Text { get; set; }
-        public Reviewer Reviewer { get; set; }
-        public Pokemon Pokemon { get; set; }
+        public Reviewer Reviewer { get; set; } // A Review belongs to a Reviewer
+        public Pokemon Pokemon { get; set; } // A Review belongs to a Pokemon
 
     }
 }
